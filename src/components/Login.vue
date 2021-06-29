ae38f6bf12428cac91a2635907b9ca96cc4b8714
Maryam <maryamrazmjoo76@yahoo.com>
2021-06-29 22:07:03 +0430

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="loginForm">
+  <form>
+    <label>Phone Number:</label>
+    <input type="number" value="Enter Your Phone Number:">
+    <label>user Name:</label>
+    <input type="password" value="Enter Your National Code">
+    <input type="submit" value="submit">
+  </form>
+
+</div>
+</template>
+
+<script>
+export default {
+name: "Login"
+}
+</script>
+
+<style scoped>
+form{
+  border: black;
+}
+
+
+</style>
