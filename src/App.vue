dabb3da7d5106507d23de83404c39e38bceafec4
kerasus <aliesmaeeli20@gmail.com>
2021-07-03 13:10:22 +0430

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
   <div>
     <router-view/>
-    <profile/>
   </div>
 
 </template>
 
 <script>
-
-import profile from "./components/profile";
-
 export default {
   name: 'App',
   data: () => ({
     //
-  }),
-  components: {
-    profile,
-  },
+  })
 };
 </script>
