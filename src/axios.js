9e798dd3bbdb82f677baeb499642a52a87bff22e
kerasus <aliesmaeeli20@gmail.com>
2021-07-03 12:56:48 +0430

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
 axios.defaults.baseURL='alaatv.com/api/v2';
-axios.defaults.headers.common['Authorization']='Bearer'+localStorage.getItem('token');
+axios.defaults.headers.common['Authorization']='Bearer '+localStorage.getItem('token');
